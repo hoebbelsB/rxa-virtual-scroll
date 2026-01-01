d56b3bcebdc0bd7340b56b1dc79a25c22fa60e6b
Julian Jandl <julian@jandl.wien>
2026-01-01 15:41:38 +0100

    refactor: input/output

--- apps/demo/src/app/demo-panel/demo-panel.component.ts
+++ apps/demo/src/app/demo-panel/demo-panel.component.ts
@@ -1,11 +1,9 @@
-
 import {
   ChangeDetectionStrategy,
   Component,
-  EventEmitter,
   inject,
-  Input,
-  Output,
+  input,
+  output,
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RxStrategyProvider } from '@rx-angular/cdk/render-strategies';
@@ -17,22 +15,22 @@ import { DataService } from '../data.service';
   template: `
     <details #details>
       <summary>Input & Stats</summary>
-    
+
       <div class="demo-panel__body">
         <div>
           <div><strong>Stats</strong></div>
           <table>
             <tr>
               <td>Items in list</td>
-              <td>{{ itemAmount }}</td>
+              <td>{{ itemAmount() }}</td>
             </tr>
             <tr>
               <td>Rendered items</td>
-              <td>{{ renderedItemsAmount }}</td>
+              <td>{{ renderedItemsAmount() }}</td>
             </tr>
             <tr>
               <td>ScrolledIndex</td>
-              <td>{{ scrolledIndex }}</td>
+              <td>{{ scrolledIndex() }}</td>
             </tr>
           </table>
         </div>
@@ -48,10 +46,10 @@ import { DataService } from '../data.service';
                   type="number"
                   step="50"
                   max="1000"
-                  />
+                />
                 <button
                   (click)="dataService.addItems(addAmountInput.valueAsNumber)"
-                  >
+                >
                   Add
                 </button>
               </td>
@@ -60,100 +58,100 @@ import { DataService } from '../data.service';
               <td>Runway items</td>
               <td>
                 <input
-                  [ngModel]="runwayItems"
+                  [ngModel]="runwayItems()"
                   (ngModelChange)="runwayItemsChange.emit($event)"
                   type="number"
                   step="1"
                   min="0"
-                  />
+                />
               </td>
             </tr>
             <tr>
               <td>Runway opposite items</td>
               <td>
                 <input
-                  [ngModel]="runwayItemsOpposite"
+                  [ngModel]="runwayItemsOpposite()"
                   (ngModelChange)="runwayItemsOppositeChange.emit($event)"
                   type="number"
                   min="0"
                   step="1"
-                  />
+                />
               </td>
             </tr>
             <tr>
               <td>viewCache</td>
               <td>
                 <input
-                  [ngModel]="viewCache"
+                  [ngModel]="viewCache()"
                   [ngModelOptions]="{ updateOn: 'blur' }"
                   (ngModelChange)="viewCacheChange.emit($event)"
                   type="number"
                   min="0"
                   step="1"
-                  />
+                />
               </td>
             </tr>
             <tr>
               <td>
                 Scroll To
-                @if (scrollToExperimental) {
+                @if (scrollToExperimental()) {
                   <span
                     title="This is probably not working correctly and should not be used in production"
                     >⚠️</span
-                    >
-                  }
+                  >
+                }
+              </td>
+              <td>
+                <input type="number" min="0" step="1" #scrollToInput />
+                <button
+                  (click)="scrollToIndex.emit(scrollToInput.valueAsNumber)"
+                >
+                  Scroll
+                </button>
+              </td>
+            </tr>
+            @if (withStableScrollbar()) {
+              <tr>
+                <td>
+                  With Stable Scrollbar
+                  <span
+                    title="This is can cause very weird effects based on the contents you are rendering. If your views are of similar size and do not change massively, you can safely use it as it increases the UX."
+                    >💡️</span
+                  >
                 </td>
                 <td>
-                  <input type="number" min="0" step="1" #scrollToInput />
-                  <button
-                    (click)="scrollToIndex.emit(scrollToInput.valueAsNumber)"
-                    >
-                    Scroll
-                  </button>
+                  <input
+                    type="checkbox"
+                    (change)="
+                      stableScrollbarChange.emit(stableScrollbarInput.checked)
+                    "
+                    [checked]="stableScrollbar()"
+                    #stableScrollbarInput
+                  />
                 </td>
               </tr>
-              @if (withStableScrollbar) {
-                <tr>
-                  <td>
-                    With Stable Scrollbar
-                    <span
-                      title="This is can cause very weird effects based on the contents you are rendering. If your views are of similar size and do not change massively, you can safely use it as it increases the UX."
-                      >💡️</span
-                      >
-                    </td>
-                    <td>
-                      <input
-                        type="checkbox"
-                  (change)="
-                    stableScrollbarChange.next(stableScrollbarInput.checked)
-                  "
-                        [checked]="stableScrollbar"
-                        #stableScrollbarInput
-                        />
-                    </td>
-                  </tr>
-                }
-                @if (withStrategy) {
-                  <tr>
-                    <td>Render Strategy</td>
-                    <td>
-                      <select
-                        [ngModel]="strategy"
-                        (ngModelChange)="strategyChange.emit($event)"
-                        >
-                        <option value="native">Native (sync)</option>
-                        <option value="immediate">Immediate</option>
-                        <option value="userBlocking">User Blocking</option>
-                        <option value="normal">Normal</option>
-                      </select>
-                    </td>
-                  </tr>
-                }
-              </table>
-            </div>
-          </div>
-        </details>
-    `,
+            }
+            @if (withStrategy()) {
+              <tr>
+                <td>Render Strategy</td>
+                <td>
+                  <select
+                    [ngModel]="strategy"
+                    (ngModelChange)="strategyChange.emit($event)"
+                  >
+                    <option value="native">Native (sync)</option>
+                    <option value="immediate">Immediate</option>
+                    <option value="userBlocking">User Blocking</option>
+                    <option value="normal">Normal</option>
+                  </select>
+                </td>
+              </tr>
+            }
+          </table>
+        </div>
+      </div>
+    </details>
+  `,
   host: {
     class: 'demo-panel',
   },
@@ -186,29 +184,27 @@ import { DataService } from '../data.service';
   imports: [FormsModule],
 })
 export class DemoPanelComponent {
-  @Input() scrollToExperimental = false;
-  @Input() withStrategy = true;
-  @Input() itemAmount = 0;
-  @Input() renderedItemsAmount = 0;
-  @Input() withStableScrollbar = false;
-  @Input() stableScrollbar = false;
-  @Output() stableScrollbarChange = new EventEmitter<boolean>();
-  @Input() scrolledIndex = 0;
-  @Output() scrollToIndex = new EventEmitter<number>();
-  @Input() runwayItemsOpposite = 5;
-  @Output() runwayItemsOppositeChange = new EventEmitter<number>();
-  @Input() viewCache = 50;
-  @Output() viewCacheChange = new EventEmitter<number>();
-  @Input() runwayItems = 20;
-  @Output() runwayItemsChange = new EventEmitter<number>();
+  readonly scrollToExperimental = input(false);
+  readonly withStrategy = input(true);
+  readonly itemAmount = input(0);
+  readonly renderedItemsAmount = input(0);
+  readonly withStableScrollbar = input(false);
+  readonly stableScrollbar = input(false);
+  readonly stableScrollbarChange = output<boolean>();
+  readonly scrolledIndex = input(0);
+  readonly scrollToIndex = output<number>();
+  readonly runwayItemsOpposite = input(5);
+  readonly runwayItemsOppositeChange = output<number>();
+  readonly viewCache = input(50);
+  readonly viewCacheChange = output<number>();
+  readonly runwayItems = input(20);
+  readonly runwayItemsChange = output<number>();
 
-  @Output() strategyChange = new EventEmitter<string>();
+  readonly strategyChange = output<string>();
 
   dataService = inject(DataService);
 
   private strategyProvider = inject(RxStrategyProvider);
 
   strategy = this.strategyProvider.primaryStrategy;
-
-  constructor() {}
 }
